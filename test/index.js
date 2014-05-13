a16a94c4f905f093747924fa7cb72c538c328bee
Michael Williams <dinosaur@riseup.net>
2014-05-12 17:25:23 -0700

    add some failing tests

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var test = require('tape');
 
 var level;
 var db;
-var Entity;
+var Entity, Creature, Unicorn;
 
 test("load level db", function (t) {
   level = require('level-test')();
@@ -29,15 +29,78 @@ test("create entities", function (t) {
   t.end();
 });
 
-test("extend Entity to Person", function (t) {
-  var Person = Entity.extend({
-    type: "Person",
+test("extend Entity to Creature", function (t) {
+  Creature = Entity.extend({
+    type: "Creature",
+    properties: {
+      name: "string",
+      life: "number",
+    },
+    methods: [{
+      live: {
+        fn: function (options, callback) {
+          var id = options.id;
+
+          this.get(id, function (err, creature) {
+            if (err) { throw err; }
+
+            creature.life += 1;
+
+            this.put(id, creature, callback);
+
+          }.bind(this))
+        },
+        input: {
+          id: {
+            type: "string",
+            required: true,
+          },
+        },
+        output: "Creature",
+      },
+    }],
+    config: {},
+  });
+  t.ok(Creature, "Creature exists");
+  t.end();
+});
+
+test("create creatures", function (t) {
+  var creatures = new Entity(db)
+  t.ok(creatures, "creatures exists");
+  t.ok(creatures.get, "creatures.get exists");
+  t.ok(creatures.put, "creatures.put exists");
+  t.ok(creatures.del, "creatures.del exists");
+  t.ok(creatures.live, "creatures.live exists");
+  t.ok(creatures.methods, "creatures.methods exists");
+  t.ok(creatures.methods.live, "creatures.methods.live exists");
+  t.equal(creatures.methods.length, 0, "creatures.methods is empty");
+  t.end();
+});
+
+test("extend Creature to Unicorn", function (t) {
+  Unicorn = Creature.extend({
+    type: "Unicorn",
     properties: {
       name: "string",
     },
     methods: [],
     config: {},
   });
-  t.ok(Person, "Person exists");
+  t.ok(Unicorn, "Unicorn exists");
   t.end();
-})
+});
+
+
+test("create unicorns", function (t) {
+  var unicorns = new Unicorn(db)
+  t.ok(unicorns, "unicorns exists");
+  t.ok(unicorns.get, "unicorns.get exists");
+  t.ok(unicorns.put, "unicorns.put exists");
+  t.ok(unicorns.del, "unicorns.del exists");
+  t.ok(unicorns.live, "unicorns.live exists");
+  t.ok(unicorns.methods, "unicorns.methods exists");
+  t.ok(unicorns.methods.live, "unicorns.methods.live exists");
+  t.equal(unicorns.methods.length, 0, "unicorns.methods is empty");
+  t.end();
+});
